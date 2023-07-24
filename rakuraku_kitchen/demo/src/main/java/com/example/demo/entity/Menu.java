6a271defad2fd9345b9aadac5a39ffa3adfe0c71
urakawa-es5 <119495449+urakawa-es5@users.noreply.github.com>
2023-07-24 10:52:58 +0900

    Create Menu.java

---
+++ rakuraku_kitchen/demo/src/main/java/com/example/demo/entity/Menu.java
@@ -0,0 +1,15 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String menu;
+
+    // コンストラクタ、getter、setterなどは省略
+}
