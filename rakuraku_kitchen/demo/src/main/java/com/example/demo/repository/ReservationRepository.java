64261bc0b5d7813fc548aac20c63758cdccac3f6
Keisuke_Kaji <keisuke3469@gmail.com>
2023-07-24 12:27:59 +0900

    reservation repo services

--- rakuraku_kitchen/demo/src/main/java/com/example/demo/repository/ReservationRepository.java
+++ rakuraku_kitchen/demo/src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -3,11 +3,10 @@ package com.example.demo.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.Reservation;
+
 @Repository
 
-public interface ReservationRepository extends CrudRepository<Tsubuyaki, Long>{
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
-  
-
-  
 }
--- rakuraku_kitchen/demo/src/main/java/com/example/demo/service/ReservationService.java
+++ rakuraku_kitchen/demo/src/main/java/com/example/demo/service/ReservationService.java
@@ -1,28 +1,31 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import com.example.demo.repository.ReservationRepository;
 import com.example.demo.entity.Reservation;
 
 @Service
 public class ReservationService {
-    private ReservationRepository reservationRepository;
+    private ReservationRepository repo;
 
-    public ReservationService(ReservationRepository reservationRepository) {
-        this.reservationRepository = reservationRepository;
+    public List<Reservation> getAllReservations() {
+        Iterable<Reservation> found = repo.findAll();
+        ArrayList<Reservation> list = new ArrayList<>();
+        found.forEach(list::add);
+        return list;
     }
 
-    public Reservation registerReservation(String name, int numberOfPeople, String dateTime,
-                                           String dish, String selectedCourse) {
+    public Reservation registerReservation(String name, int numberOfPeople, String dateTime, String selectedCourse) {
         // Entityのインスタンスを作成して予約情報を登録
         Reservation reservation = new Reservation();
         reservation.setName(name);
         reservation.setNumberOfPeople(numberOfPeople);
         reservation.setDateTime(dateTime);
-        reservation.setDish(dish);
         reservation.setSelectedCourse(selectedCourse);
-
-        return reservationRepository.save(reservation);
+        return repo.save(reservation);
     }
 
     // 他に必要なビジネスロジックを追加する
