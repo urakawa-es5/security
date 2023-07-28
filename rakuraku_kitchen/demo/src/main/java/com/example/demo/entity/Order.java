d12d4dbcf38d409f45da9b5823f7f30bb434cd0c
urakawa-es5 <119495449+urakawa-es5@users.noreply.github.com>
2023-07-28 14:43:39 +0900

    Update Orders.java

--- rakuraku_kitchen/demo/src/main/java/com/example/demo/entity/Order.java
+++ rakuraku_kitchen/demo/src/main/java/com/example/demo/entity/Order.java
@@ -12,20 +12,44 @@ import lombok.Data;
 
 @Data
 @Entity
-public class Order {
+public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
-    private Long dishid;
-    private int dishcount;
+
+
+
     @Temporal(TemporalType.TIMESTAMP)
     Date ordertime; //作成日時
-    private String tukuenumber;
-    private String order;
-    private String orderstatus;
-    private String reservationflag;
 
+    private String dishname;
+    private String status; 
+
+    public String getDishname() {
+        return dishname;
+    }
+    public void setDishname(String dishname) {
+        this.dishname = dishname;
+    }
+        public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Date getOrdertime() {
+        return ordertime;
+    }
+    public void setOrdertime(Date ordertime) {
+        this.ordertime = ordertime;
     // コンストラクタ、getter、setterなどは省略
+    }
+
 }
 
